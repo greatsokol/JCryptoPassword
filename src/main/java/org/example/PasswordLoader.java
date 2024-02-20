1002b4b82a8083301d9a95262a41897f863fcdd5
Eugene Sokolov <greatsokol@gmail.com>
2024-02-20 18:40:36 +0300

    simplyfied PasswordLoader

--- src/main/java/org/example/PasswordLoader.java
+++ src/main/java/org/example/PasswordLoader.java
@@ -1,15 +1,12 @@
 package org.example;
 
+import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 
 public class PasswordLoader {
     private PasswordLoader() {
@@ -22,63 +19,27 @@ public class PasswordLoader {
                               String passwordAlias,
                               String passwordPassword
                               ) {
-        KeyStore keyStore;
-        try {
-            keyStore = KeyStore.getInstance(keyStoreType);
-        } catch (KeyStoreException ex) {
-           System.out.println("KeyStore instance not created" + ex.getMessage());
-           return null;
-        }
+        try{
+            InputStream keyStoreData = Files.newInputStream(Paths.get(keyStorePath));
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(keyStoreData, keyStorePassword.toCharArray());
 
-        try {
-            try (InputStream keyStoreData = Files.newInputStream(Paths.get(keyStorePath))) {
-                try {
-                    keyStore.load(keyStoreData, keyStorePassword.toCharArray());
-                } catch (Exception ex) {
-                    System.out.println("Keystore not loaded "+ex.getMessage());
-                    return null;
-                }
-            }
-        } catch (IOException ex) {
-            System.out.println("File not loaded "+ex.getMessage());
-            return null;
-        }
-
-//        try {
 //            Enumeration<String> aliases = keyStore.aliases();
 //            while (aliases.hasMoreElements()) {
 //                System.out.println(aliases.nextElement());
 //            }
-//        } catch (KeyStoreException ex) {
-//            System.out.println(ex.getMessage());
-//            return null;
-//        }
 
-        KeyStore.SecretKeyEntry secretKeyEntry;
-        KeyStore.ProtectionParameter entryPassword = new KeyStore.PasswordProtection(passwordPassword.toCharArray());
-        try {
-            secretKeyEntry = (KeyStore.SecretKeyEntry) keyStore.getEntry(passwordAlias, entryPassword); //"mykey"
+            KeyStore.ProtectionParameter entryPassword = new KeyStore.PasswordProtection(passwordPassword.toCharArray());
+            KeyStore.SecretKeyEntry secretKeyEntry = (KeyStore.SecretKeyEntry) keyStore.getEntry(passwordAlias, entryPassword); //"mykey"
+
+            SecretKey secretKey = secretKeyEntry.getSecretKey();
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(secretKey.getAlgorithm());
+            PBEKeySpec keySpec = (PBEKeySpec) factory.getKeySpec(secretKey, PBEKeySpec.class);
+
+            return new String(keySpec.getPassword());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
             return null;
         }
-
-        SecretKeyFactory factory;
-        try {
-            factory = SecretKeyFactory.getInstance(secretKeyEntry.getSecretKey().getAlgorithm());
-        } catch (NoSuchAlgorithmException ex) {
-            System.out.println(ex.getMessage());
-            return null;
-        }
-
-        PBEKeySpec keySpec;
-        try {
-            keySpec = (PBEKeySpec) factory.getKeySpec(secretKeyEntry.getSecretKey(), PBEKeySpec.class);
-        } catch(InvalidKeySpecException ex) {
-            System.out.println(ex.getMessage());
-            return null;
-        }
-
-        return new String(keySpec.getPassword());
     }
 }
